fee9b4b295565b03e0373b065776cf596fe4d93d
Sam Bennrubi <sbennrubi@gmail.com>
2018-10-30 23:08:50 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -3,3 +3,35 @@ def using_push(array, string)
   next_element = string
   array.push(next_element)
 end
+
+def using_unshift
+
+end
+
+def using_pop
+  
+end
+
+def pop_with_args
+  
+end
+
+def using_shift
+  
+end
+
+def shift_with_args
+  
+end
+
+def using_concat
+  
+end
+
+def using_insert
+  
+end
+
+def using_uniq
+
+end
